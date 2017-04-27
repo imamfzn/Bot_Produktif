06316a31acf9d8de805d22b4640d06633f77944d
Imam Fauzan <imamfzn@gmail.com>
2017-04-27 14:48:02 +0700

    init spring project + line dependency

---
+++ src/test/java/com/bot_produktif/BotProduktifApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bot_produktif;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotProduktifApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
