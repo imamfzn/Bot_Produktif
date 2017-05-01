0822f385f3619f249496bede6857fed49ef02007
exagradia <exagradia@outlook.com>
2017-05-01 23:11:15 +0700

    Change @RequestMapping ("/") to @RequestMapping ("/helloworld")

--- src/main/java/com/bot_produktif/SampleController.java
+++ src/main/java/com/bot_produktif/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/helloworld")
 	public String HelloWorld()
 	{
 		return "Hello World";
