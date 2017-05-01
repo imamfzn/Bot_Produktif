8dd2a107d07b6715ad51a4511ee8a656de52e08f
exagradia <exagradia@outlook.com>
2017-05-01 19:33:47 +0700

    Add HelloWorld() method

--- src/main/java/com/bot_produktif/SampleController.java
+++ src/main/java/com/bot_produktif/SampleController.java
@@ -1,5 +1,16 @@
 package com.bot_produktif;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class SampleController {
+	
+	@RequestMapping("/")
+	public String HelloWorld()
+	{
+		return "Hello World";
+	}
 
 }
