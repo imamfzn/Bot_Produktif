7dbce65f116e83204e9cc020250b58aa241f5e5d
Imam Fauzan <imamfzn@gmail.com>
2017-05-31 10:29:42 +0700

    add contructor with adzan model

--- src/main/java/com/yukproduktif/model/PrayerTimesView.java
+++ src/main/java/com/yukproduktif/model/PrayerTimesView.java
@@ -47,6 +47,25 @@ public class PrayerTimesView {
 		textMessage = "";
 	}
 	
+	public PrayerTimesView(PrayerTimes jadwalAdzan){
+		//create view carousel message
+		actions = Arrays.asList(new URIAction("Yuk Shalat! :)", "https://line.me/R/ti/p/%40LTA5871H")); //dumy button
+		
+		columns = Arrays.asList(
+				new CarouselColumn(IMAGE_URL_SHUBUH,"Waktu Shubuh","04:30 am", actions),
+				new CarouselColumn(IMAGE_URL_DZUHUR,"Waktu Dzuhur","04:30 am", actions),
+				new CarouselColumn(IMAGE_URL_ASHAR,"Waktu Ashar","04:30 am", actions),
+				new CarouselColumn(IMAGE_URL_MAGRIB,"Waktu Magrib","04:30 am", actions),
+				new CarouselColumn(IMAGE_URL_ISYA,"Waktu Isya","04:30 am", actions)
+		);
+		
+		carousel = new CarouselTemplate(columns);
+		viewMessage = new TemplateMessage(TITLE, carousel);
+		
+		//create text message format..
+		textMessage = "";
+	}
+	
 	public TemplateMessage getViewMessage(){
 		return viewMessage;
 	}
