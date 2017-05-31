8a5d26d4d4ac095e413992a0fa0421b7300c3085
Imam Fauzan <imamfzn@gmail.com>
2017-05-31 12:48:06 +0700

    creat bot remindermodel

---
+++ src/main/java/com/yukproduktif/model/BotReminder.java
@@ -0,0 +1,10 @@
+package com.yukproduktif.model;
+
+public class BotReminder {
+	private String userId;
+	private boolean shubuh;
+	private boolean dzuhur;
+	private boolean ashar;
+	private boolean magrib;
+	private boolean isya;
+}
