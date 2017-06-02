f2c7a369b234a9c1838ae35395b878b7b2c6a50e
Imam Fauzan <imamfzn@gmail.com>
2017-06-02 11:12:34 +0700

    change img url

--- src/main/java/com/yukproduktif/model/ReminderSunnahView.java
+++ src/main/java/com/yukproduktif/model/ReminderSunnahView.java
@@ -12,8 +12,8 @@ import com.linecorp.bot.model.message.template.Template;
 public class ReminderSunnahView {
 	
 	private final static String TITLE = "Setting Reminder Sunnah";
-	private final static String IMAGE_URL_DHUHA = "https://i2.wp.com/dalamruangkeheningan.files.wordpress.com/2017/05/1.jpg?w=463&h=307&crop&ssl=1";
-	private final static String IMAGE_URL_TAHAJUD = "https://i0.wp.com/dalamruangkeheningan.files.wordpress.com/2017/05/2.jpg?w=229&h=152&crop&ssl=1";
+	private final static String IMAGE_URL_DHUHA = "https://dalamruangkeheningan.files.wordpress.com/2017/05/designf.jpg";
+	private final static String IMAGE_URL_TAHAJUD = "https://dalamruangkeheningan.files.wordpress.com/2017/05/designh.jpg";
 	
 	private List<CarouselColumn> columns;
 	private Template carousel;
@@ -22,8 +22,8 @@ public class ReminderSunnahView {
 	public ReminderSunnahView(){
 		//create view carousel message				
 		columns = Arrays.asList(
-			new CarouselColumn(IMAGE_URL_DHUHA,"Reminder Dhuha","08:30 am", Arrays.asList(new MessageAction("Aktifkan", "reminder sunnah dhuha on"))),
-			new CarouselColumn(IMAGE_URL_TAHAJUD,"Reminder Tahajud","02:30 am", Arrays.asList(new MessageAction("Aktifkan", "reminder sunnah tahajud on")))
+			new CarouselColumn(IMAGE_URL_DHUHA,"Reminder Dhuha","08:30", Arrays.asList(new MessageAction("Aktifkan", "reminder sunnah dhuha on"))),
+			new CarouselColumn(IMAGE_URL_TAHAJUD,"Reminder Tahajud","02:30", Arrays.asList(new MessageAction("Aktifkan", "reminder sunnah tahajud on")))
 		);
 				
 		carousel = new CarouselTemplate(columns);
